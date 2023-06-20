066fb349580c0df7bdf7a27c884e86c7a657e868
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-20 18:31:26 +0100

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ *
+ * @i, j: integer input
+ *
+ * Return: n
+ */
+int	add(int i, int j)
+{
+	int n = i + j;
+
+	return n;
+}
