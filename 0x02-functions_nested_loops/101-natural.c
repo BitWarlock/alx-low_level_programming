21f9a6c477883f08a24ae40e73bcb177d79742ff
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-21 00:50:54 +0100

    prints the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints the sum of all the multiples of 3 or 5 below 1024.
+ *
+ * Return: 0 success.
+ */
+
+int	main(void)
+{
+	int	sum;
+
+	sum = 0;
+	for (int counter = 1; counter < 1024; counter++)
+	{
+		if (counter % 3 == 0 || counter % 5 == 0)
+		{
+			sum = counter + sum;
+		}
+	}
+	printf("The total is %d\n", sum);
+	return (0);
+}
