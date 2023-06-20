313a8087b0683bb5eeb20ceb183ffef47e806212
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-20 18:20:52 +0100

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,29 +5,29 @@
  *
  * Return: void.
  */
-void times_table(void)
+void	times_table(void)
 {
-    int i, j, d;
+	int i, j, d;
 
-    for (i = 0; i < 10; i++)
-    {
-        _putchar('0');
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
 
-        for (j = 1; j < 10; j++)
-        {
-            _putchar(',');
-            _putchar(' ');
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
 
-            d = i * j;
+			d = i * j;
 
-            if (d <= 9)
-                _putchar(' ');
-            else
-                _putchar((d / 10) + '0');
+			if (d <= 9)
+				_putchar(' ');
+			else
+				_putchar((d / 10) + '0');
 
-            _putchar((d % 10) + '0');
-        }
+			_putchar((d % 10) + '0');
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
