97c96082a95c1d21d4c69e24c0dc53810ba44630
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-20 10:11:34 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+int	main(void)
+{
+	write(1,"_putchar\n", 9);
+	return (0);
+}
