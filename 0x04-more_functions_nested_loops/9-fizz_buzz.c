36c849f3dd2d7997f48b87863c2885f143cda686
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-22 14:37:53 +0100

    FizzBuzz error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,14 +15,22 @@ int	main(void)
 	i = 1;
 	while (i <= 100)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 5) != 0)
+		{
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		}
+		else if ((i % 5) == 0 && (i % 3) != 0)
+		{
 			printf("Buzz ");
-		if ((i % 3) == 0 && (i % 5) == 0)
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 		i++;
 	}
 	putchar('\n');
