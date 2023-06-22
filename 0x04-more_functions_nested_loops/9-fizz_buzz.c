caa636bc4a6cc31e3ae155fbe310c04d10e7f6a0
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-22 14:23:53 +0100

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - prints numbers from 1 - 100.
+ * description: if number is a multiplies of 3 print fizz, for 5 buzz, for both
+ * fizzbuzz.
+ * Return: Always success 0.
+ */
+
+int	main(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
