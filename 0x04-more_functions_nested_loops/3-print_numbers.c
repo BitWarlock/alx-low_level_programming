365ecc85acf1282a247875dc7d798ab8cb0dd233
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-22 12:03:39 +0100

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 - 9.
+ *
+ * Return: void.
+ */
+
+void	print_numbers(void)
+{
+	int	i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+}
