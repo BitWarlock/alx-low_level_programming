058a5bd4cf00634ec3018b7b9af471f115eb1e49
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-22 13:01:55 +0100

    draws a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line: draws a straight line in the terminal.
+ * @n: number of lines.
+ * Return: void.
+ */
+
+void	print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int	i;
+
+		i = 0;
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
