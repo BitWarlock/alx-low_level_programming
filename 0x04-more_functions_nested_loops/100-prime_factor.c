34f4fb04863bbc75fdb332b7b95571ec5b1e072a
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-22 15:19:00 +0100

    primes

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+/**
+ * main - prints the largest prime factor of the number 612852475143.
+ * 
+ * Return: 0 Always success.
+ */
+
+int	main(void)
+{
+	uint64_t	n;
+	uint64_t	divisor;
+
+	n = 600851475143;
+	divisor = 1;
+	while (n != 1)
+	{
+		divisor += 2;
+		while (!(n % divisor))
+			n /= divisor;
+	}
+	printf("The largest prime factor is %lu\n", divisor);
+	return (0);
+}
