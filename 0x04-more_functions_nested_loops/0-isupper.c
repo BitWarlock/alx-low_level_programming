d7fb9fb194e799fdcdda6a844bf45edd6b835e9d
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-06-22 11:39:50 +0100

    is upper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is uppercase or not.
+ *@c: character.
+ *
+ * Return: 1 if uppercase, 0 if not.
+ */
+
+int	_isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+		return (1);
+	else
+		return (0);
+}
