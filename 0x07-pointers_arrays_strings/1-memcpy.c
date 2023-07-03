c39d7844382f1d521beb21df4958068849c5a672
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-07-03 19:40:41 +0100

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: n bytes to copy.
+ * Return: pointer to dest.
+ */
+
+char	*_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
