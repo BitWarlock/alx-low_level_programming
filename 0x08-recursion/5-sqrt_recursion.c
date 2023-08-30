2c650a48c69b5a64bc4352b14c76b41e0d500c3e
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-08-30 20:51:39 +0100

    recursive sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sqrt69 - Recursive helper for calculating square root.
+ * @n: number.
+ * @a: number.
+ * Return: square root of n.
+ */
+
+int	sqrt69(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	else
+		return (sqrt69(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number.
+ * @n: number.
+ * Return: square root of n.
+ */
+
+int	_sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (sqrt69(n, 0));
+}
