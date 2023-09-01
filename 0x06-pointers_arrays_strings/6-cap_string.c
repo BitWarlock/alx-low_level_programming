5683a3e0928d150c7e397961a5ded215176b70a7
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-01 16:17:40 +0100

    rm: comments

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -28,27 +28,3 @@ char *cap_string(char *s)
 	}
 	return (s);
 }
-
-/*
-
-char	*cap_string(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' ||
-				s[i] == ';' || s[i] == '.' || s[i] == '!' ||
-				s[i] == '?' || s[i] == '"' || s[i] == '(' ||
-				s[i] == ')' || s[i] == '{' || s[i] == '}')
-		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s[i + 1] -= 32;
-		}
-		i++;
-	}
-	return (s);
-}
-
-*/
