57bee94bcf74a595177bfb5eb3a11c4117f85289
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-01 17:37:32 +0100

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point.
+ * @argc: argument count.
+ * @argv: argument verctor.
+ * Return: always 0.
+ */
+
+int	main(int argc, char **argv)
+{
+	int	a;
+	int	b;
+	int	res;
+
+	a = 1;
+	res = 0;
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		while (argv[a] && a <= argc)
+		{
+			b = 0;
+			while (argv[a][b])
+			{
+				if (argv[a][b] < '0' || argv[a][b] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+				b++;
+			}
+			res += atoi(argv[a]);
+			a++;
+		}
+		printf("%d\n", res);
+	}
+	return (0);
+}
