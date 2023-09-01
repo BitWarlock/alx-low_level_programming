e231f98a98e57b3c0720bbd5809838a427c3a6ba
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-01 15:03:14 +0100

    fix betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: string one.
  * @s2: string two.
  * Return: integer indicating the result of the comparison.
- */
+*/
 
 int	_strcmp(char *s1, char *s2)
 {
@@ -13,7 +13,7 @@ int	_strcmp(char *s1, char *s2)
 
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i])
-			i++;
+		i++;
 	if (s1[i] == s2[i])
 		return (0);
 	return (s1[i] - s2[i]);
