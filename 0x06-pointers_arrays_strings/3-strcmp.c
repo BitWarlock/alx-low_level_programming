8eb33502e74244fef94720fc68183c0220ebb542
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-01 14:47:59 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: string one.
+ * @s2: string two.
+ * Return: integer indicating the result of the comparison.
+ */
+
+int	_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+			i++;
+	if (s1[i] == s2[i])
+		return (0);
+	return (s1[i] - s2[i]);
+}
