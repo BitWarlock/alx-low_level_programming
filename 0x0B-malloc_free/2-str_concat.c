3f2646a5c13ed8fa59ceb67d1ee502ad58db0742
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-05 17:30:41 +0100

    remove ternary

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,11 +24,10 @@ char	*str_concat(char *s1, char *s2)
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 		return (NULL);
-	while (i < len1 && (ptr[i] = s1[i], 1))
-		i++;
-	i = 0;
-	while (i < len2 && (ptr[len1 + i] = s2[i], 1))
-		i++;
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+	for (i = 0; i < len2; i++)
+	    ptr[len1 + i] = s2[i];
 	ptr[len1 + len2] = '\0';
 	return (ptr);
 }
