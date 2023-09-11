d13e8d7bbadccbce57eaa0d65129e3ed28081963
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-11 23:26:51 +0100

    add first task

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct for a dog.
+ * @name: name of the dog.
+ * @age: dog age.
+ * @owner: dog's owner.
+ */
+
+typedef struct	dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+}	dog_t;
+
+#endif
