f036bec3a1257e220dd52702af757194d8a3a145
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-21 23:27:50 +0100

    btdse

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,19 +7,30 @@
  * Return: addrss to new node.
  */
 
-list_t	*add_node(list_t **head, const char *str)
+#include "lists.h"
+#include <string.h>
+
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the head of the list
+ * @str: string to be duplicated
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t	*new_node;
+	list_t *new;
 	int	len = 0;
 
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
 	while (str[len])
 		len++;
-	new_node->str = strdup(str);
-	new_node->len = len;
-	new_node->next = (*head);
-	(*head) = new_node;
-	return (new_node);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
