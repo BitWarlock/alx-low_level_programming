486407d3edd9fdd88ec34ad3f30a5d6626ef2163
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-21 23:19:03 +0100

    third task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 
 /**
- * add_node - adds new node to a linked list.
- * @head: pointer 
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @head: pointer to the head.
+ * @str: string.
+ * Return: addrss to new node.
+ */
+
+list_t	*add_node(list_t **hed, const char *str)
+{
+	list_t	*new_node;
+	int	len = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	while (str[len])
+		len++;
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = (*head);
+	(*head) = new_node;
+	return (new_node);
+}
