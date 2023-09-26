38fbcce0c9cd7522b0d970635f1cb5f433a5c913
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-26 17:24:28 +0100

    first task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 
 size_t	print_listint(const listint_t *h)
 {
-    size_t nnodes = 0;
+	size_t	nnodes = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        nnodes++;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
 
-    return (nnodes);
+	return (nnodes);
 }
