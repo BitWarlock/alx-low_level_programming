54ec8c1078baa3848e7098ddf7b13d067b2d3cd2
BitWarlock <simorezki@outlook.com>
2024-02-24 11:01:41 +0100

    fix return

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,8 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-		text_content = "";
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | O_APPEND, 0600);
+		return (1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
 	while (text_content[len])
