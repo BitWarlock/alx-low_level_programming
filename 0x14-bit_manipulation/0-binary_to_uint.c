67f322ec31c7ecfdc7a2ed4b723a82e147a6c5ed
BitWarlock <simorezki@outlook.com>
2024-02-24 04:24:49 +0100

    add first

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+#include <stdio.h>
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: binary.
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int	ui;
+	int	len;
+	int	base;
+
+	len = -1;
+	base = 1;
+	ui = 0;
+	if (!b)
+		return (0);
+	while (b[++len])
+		;
+	while (len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		if (b[len] & 1)
+			ui += base;
+		base *= 2;
+	}
+	return (ui);
+}
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    unsigned int n;
+
+    n = binary_to_uint("1");
+    printf("%u\n", n);
+    n = binary_to_uint("101");
+    printf("%u\n", n);
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);
+    return (0);
+}
