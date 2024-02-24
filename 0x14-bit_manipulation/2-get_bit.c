014174ebd6c60bc95cfe43750cdcfe800983c223
BitWarlock <simorezki@outlook.com>
2024-02-24 05:02:29 +0100

    fix betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,24 +15,3 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (1);
 	return (0);
 }
-//
-// #include <stdio.h>
-// #include "main.h"
-//
-// /**
-//  * main - check the code
-//  *
-//  * Return: Always 0.
-//  */
-// int main(void)
-// {
-//     int n;
-//
-//     n = get_bit(1024, 10);
-//     printf("%d\n", n);
-//     n = get_bit(98, 1);
-//     printf("%d\n", n);
-//     n = get_bit(1024, 0);
-//     printf("%d\n", n);
-//     return (0);
-// }
