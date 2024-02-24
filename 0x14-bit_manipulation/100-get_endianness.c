9f75a3b0ceeabeefd0e428c49013c8585d79eec1
BitWarlock <simorezki@outlook.com>
2024-02-24 06:09:19 +0100

    add get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* get_endianness - function that checks the endianness.
+* Return: 0 if big endian, else 1.
+*/
+
+int get_endianness(void)
+{
+	int	n;
+
+	n = 1;
+	if(*(char *)&n == 1)
+		return (1);
+	return (0);
+}
