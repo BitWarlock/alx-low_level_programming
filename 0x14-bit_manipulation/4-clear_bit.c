1bd1d1744d6cf682c9259f55b6d6ab40023e0895
BitWarlock <simorezki@outlook.com>
2024-02-24 05:18:20 +0100

    add clear_bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,4 +2,15 @@
 
 /**
 * clear_bit -  function that sets the value of a bit to 0 at a given index.
-* @
+* @n: integer.
+* @index: index of the interger.
+* Return: 1 if success, -1 otherwise.
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
