72a25988eff3318c3e89371da8077623b01e77ff
BitWarlock <simorezki@outlook.com>
2024-02-24 04:35:34 +0100

    remove main and add 2nd ex

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-#include <stdio.h>
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: binary.
@@ -30,26 +28,27 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (ui);
 }
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    unsigned int n;
-
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
-}
+// #include <stdio.h>
+// #include "main.h"
+//
+// /**
+//  * main - check the code
+//  *
+//  * Return: Always 0.
+//  */
+// int main(void)
+// {
+//     unsigned int n;
+//
+//     n = binary_to_uint("1");
+//     printf("%u\n", n);
+//     n = binary_to_uint("101");
+//     printf("%u\n", n);
+//     n = binary_to_uint("1e01");
+//     printf("%u\n", n);
+//     n = binary_to_uint("1100010");
+//     printf("%u\n", n);
+//     n = binary_to_uint("0000000000000000000110010010");
+//     printf("%u\n", n);
+//     return (0);
+// }
