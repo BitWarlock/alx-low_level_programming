23456129992a4964520cb43e541a0047531ac402
BitWarlock <simorezki@outlook.com>
2024-02-24 10:46:42 +0100

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,24 +9,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t	res;
+	ssize_t	res, fd, rd;
 	char	*str;
-	int	fd;
 
 	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
 		return (0);
 	str = malloc(sizeof(char) * letters);
 	if (!str)
 		return (0);
-	res = read(fd, str, letters);
-	if (res < 0)
+	fd = open(filename, O_RDONLY);
+	rd = read(fd, str, letters);
+	res = write(1, str, rd);
+	if (res < 0 || fd < 0 || rd < 0 || rd != res)
+	{
+		free(str);
 		return (0);
-	res = write(1, str, res);
-	if (res < 0)
-		return (0);
+	}
 	close(fd);
 	free(str);
 	return (res);
